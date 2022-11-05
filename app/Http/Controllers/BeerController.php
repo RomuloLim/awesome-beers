d5fecb44ff08963ffab9cbaf34a83447cc5bcb97
RomuloLim <romulo.lf123@gmail.com>
2022-11-05 17:28:37 -0300

    minio upload ready

--- app/Http/Controllers/BeerController.php
+++ app/Http/Controllers/BeerController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Exports\BeerExport;
 use App\Http\Requests\BeerRequest;
 use App\Services\PunkApiService;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Storage;
 use Maatwebsite\Excel\Facades\Excel;
 
 class BeerController extends Controller
@@ -26,11 +28,16 @@ class BeerController extends Controller
             return collect($item)->only(['name', 'tagline', 'first_brewed', 'description']);
         })->toArray();
 
+        $fileName = 'exportedBeers_'.Carbon::now()->toISOString().'.xlsx';
+
         Excel::store(
             new BeerExport($filteredBeers),
-            'aoba.xlsx',
+            $fileName,
             's3');
 
-        return 'relatorio criado';
+        //TODO: resolve url error
+        return response()->json([
+            'url' => Storage::url($fileName),
+        ]);
     }
 }
