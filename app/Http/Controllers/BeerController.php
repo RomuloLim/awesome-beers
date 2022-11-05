cdef164f40c501116bde7be9b14570ad7eaded9c
RomuloLim <romulo.lf123@gmail.com>
2022-11-05 17:15:16 -0300

    laravel excel

--- app/Http/Controllers/BeerController.php
+++ app/Http/Controllers/BeerController.php
@@ -2,18 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Exports\BeerExport;
 use App\Http\Requests\BeerRequest;
 use App\Services\PunkApiService;
+use Maatwebsite\Excel\Facades\Excel;
 
 class BeerController extends Controller
 {
-    public function index(BeerRequest $request, PunkApiService $service)
+    public function __construct(
+        protected PunkApiService $service
+    ){}
+
+    public function index(BeerRequest $request)
     {
-        return $service->getBeers(...$request->validated());
+        return $this->service->getBeers(...$request->validated());
     }
 
-    public function export()
+    public function export(BeerRequest $request)
     {
-        return 'bomdia';
+        $beers = $this->service->getBeers(...$request->validated());
+
+        $filteredBeers = collect($beers)->map(function($item, $key){
+            return collect($item)->only(['name', 'tagline', 'first_brewed', 'description']);
+        })->toArray();
+
+        Excel::store(
+            new BeerExport($filteredBeers),
+            'aoba.xlsx',
+            's3');
+
+        return 'relatorio criado';
     }
 }
